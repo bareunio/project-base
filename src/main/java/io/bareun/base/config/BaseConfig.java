f9fd87918f1486f5c605e78db26f1160bb15dc16
10 <dinophy@nate.com>
2024-11-11 14:28:06 +0900

    fix: Spring boot 비활성화로 인한 의존성 간소화 (#69)

---
+++ src/main/java/io/bareun/base/config/BaseConfig.java
@@ -0,0 +1,9 @@
+package io.bareun.base.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("io.bareun.base")
+public class BaseConfig {
+}
