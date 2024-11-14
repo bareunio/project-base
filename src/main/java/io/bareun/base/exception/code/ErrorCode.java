b3d42e008588d99f28d5d2b8ad1723015f7c703e
10 <dinophy@nate.com>
2024-11-14 11:06:29 +0900

    :recycle: 예외처리 시, 유틸 클래스 formatMessage 메서드 호출로 변경 (#74)

--- src/main/java/io/bareun/base/exception/code/ErrorCode.java
+++ src/main/java/io/bareun/base/exception/code/ErrorCode.java
@@ -5,12 +5,4 @@ public interface ErrorCode {
     int getCode();
 
     String getMessage();
-
-    default String getFormattedMessage(Object... args) {
-        if (args == null || args.length == 0) {
-            return getMessage().replaceAll("%[a-zA-Z]", "").strip();
-        }
-
-        return String.format(getMessage(), args);
-    }
 }
